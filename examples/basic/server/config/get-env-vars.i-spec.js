d196c9ee02208db5e15d220e83b00376a8154ded
Markus Daehn <markus.daehn@outlook.com>
2016-11-21 14:19:32 -0800

    removed unnnecesary console.log

--- examples/basic/server/config/get-env-vars.i-spec.js
+++ examples/basic/server/config/get-env-vars.i-spec.js
@@ -4,12 +4,13 @@ const getEnvVars = require('./get-env-vars');
 const path = require('path');
 
 describe('server config get-env-vars -- integration', () => {
-  const DEFAULT_SERVER_ROOT = path.normalize(__dirname + '../');
+  const DEFAULT_SERVER_ROOT = path.resolve(__dirname + '../');
   const EXPECTED_DEFAULT_CONFIG = {
       SERVER_ROOT: DEFAULT_SERVER_ROOT,
       IP: undefined,
       PORT: 8080,
       NODE_ENV: undefined,
+      LOG_LEVEL: 'error',
       LOG_PATH: `${DEFAULT_SERVER_ROOT}/logs/log.txt`
   };
 
