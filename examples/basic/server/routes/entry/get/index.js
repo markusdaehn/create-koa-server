cebc654ce6f275352200429c91cc077da67ab022
Markus Daehn <markus.daehn@outlook.com>
2016-11-21 08:41:01 -0800

    added config to basic example and added warning to readme.

--- examples/basic/server/routes/entry/get/index.js
+++ examples/basic/server/routes/entry/get/index.js
@@ -1,3 +1,4 @@
-const route = require('./create')(console.log.bind(console));
+const entryService = require('../../../../application/services/entry-service');
+const route = require('./create')(console.log.bind(console), );
 
 module.exports = route;
