8faf63e5b8503e399a48faac34e62698ff2744ab
Markus Daehn <markus.daehn@outlook.com>
2016-12-05 22:18:33 -0800

    updated error-handler hook implementation to log info for default/hook and fix bug with it

--- server/middleware/hooks/error-handler/get-handler.js
+++ server/middleware/hooks/error-handler/get-handler.js
@@ -1,5 +1,11 @@
-module.exports = function getHandler(getHooks, defaultHandler, server, logger) {
+module.exports = function getHandler(getObjectKeys, getHooks, defaultHandler, server, logger) {
   let hooks = getHooks(server, logger);
+  let hookNames = getObjectKeys(hooks);
 
-  return hooks.length > 0 ? hooks[0] : defaultHandler;
+  if(hookNames.length > 0) {
+    logger.info({func: 'server.middleware.hooks.error-handler.get-handler'}, `${hooksNames[0]} error handler returned`);
+  } else {
+    logger.info({func: 'server.middlware.hooks.error-handler.get-handler'}, 'Default error handler returned');
+    return defaultHandler;
+  }
 }
--- server/middleware/hooks/error-handler/index.js
+++ server/middleware/hooks/error-handler/index.js
@@ -6,7 +6,7 @@ const toCamelCase = require('../../helpers/to-camel-case');
 const getDirectories = R.curry(require('../../helpers/get-directories'))(fs, path, constants.HOOKS_ERROR_HANDLER_FOLDER);
 const getHooks = R.curry(require('../../helpers/get-middlewares'))(getDirectories, toCamelCase);
 const handleError = require('./handle-error');
-const getHandler = R.curry(require('./get-handler'))(getHooks, handleError);
+const getHandler = R.curry(require('./get-handler'))(Object.keys, getHooks, handleError);
 const createErrorHandler = R.curry(require('./create'))(getHandler);
 
 module.exports = {
