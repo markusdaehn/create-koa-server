1bc3f10e84add7a5f9e410e5b64fdcfc7e9fe5e8
Markus Daehn <markus.daehn@outlook.com>
2016-12-05 22:48:25 -0800

    fixed misspelled middleware in constant folder name for error-handler

--- server/middleware/helpers/constants.js
+++ server/middleware/helpers/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
   PLUGINS_FOLDER: '/middleware/plugins',
   HOOKS_ROUTER_FOLDER: '/middleware/hooks/router',
-  HOOKS_ERROR_HANDLER_FOLDER: '/middlware/hooks/error-handler'
+  HOOKS_ERROR_HANDLER_FOLDER: '/middleware/hooks/error-handler'
 });
