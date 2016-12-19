de001c43374e0076787d4f02c2b707e0781482a2
Markus Daehn <markus.daehn@outlook.com>
2016-12-19 00:45:16 -0800

    fixed get routes not having the correct params

--- server/middleware/hooks/router/init-routes.js
+++ server/middleware/hooks/router/init-routes.js
@@ -1,8 +1,8 @@
 
-module.exports = function initRoutes(getRoutes, ROUTES_FOLDER, router, app, logger) {
+module.exports = function initRoutes(getRoutes, router, app, logger) {
   logger.trace(`server.middleware.hooks.router.initRoutes > : root=${app.root}`);
 
-  getRoutes(ROUTES_FOLDER, app, logger).forEach(function(route) {
+  getRoutes(app, logger).forEach(function(route) {
     logger.debug(`server.middleware.hooks.router.initRoutes: registering route with verb ${route.verb} and uri template ${route.uriTemplate}`)
     router[route.verb](route.uriTemplate, route.endpoint);
   });
