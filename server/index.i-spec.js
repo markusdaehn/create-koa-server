36468aa95eefe4a1227a5ba97299b9b11aacd22c
Markus Daehn <markus.daehn@outlook.com>
2016-12-18 17:10:22 -0800

    added server index test

---
+++ server/index.i-spec.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const sinon = require('sinon');
+describe('server -- integration', () => {
+  let sandbox;
+  let config;
+  let logger;
+  let server;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    logger = createLogger(sandbox);
+    config = createConfig(sandbox);
+  });
+
+  context('when calling the server.create method', () => {
+    it('should create server', () => {
+      server = require('./')(config, logger);
+    });
+  });
+});
+
+function createConfig() {
+  let port = 8080;
+  let ip = '156.129.55.01';
+  let root = path.resolve(__dirname, '../tests/scenarios/basic-server');
+
+  return {
+    server: {
+      ip,
+      port,
+      root
+    }
+  };
+}
+function createLogger(sandbox) {
+  return {
+    debug: sandbox.stub(),
+    error: sandbox.stub(),
+    warn: sandbox.stub(),
+    trace: sandbox.stub(),
+    info: sandbox.stub()
+  };
+}
