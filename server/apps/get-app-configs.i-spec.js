4c02f5b090bb6a65bb8cca558a40522bc4a6de93
Markus Daehn <markus.daehn@outlook.com>
2016-12-20 23:12:30 -0800

    refactor getAppsConfig to return default

--- server/apps/get-app-configs.i-spec.js
+++ server/apps/get-app-configs.i-spec.js
@@ -5,24 +5,26 @@ const fs = require('fs');
 const sinon = require('sinon');
 const constants = require('../helpers/constants');
 const getDirectories = R.curry(require('../helpers/get-directories'))(fs, path);
-const getMountConfigs = R.curry(require('./get-app-configs'))(path, getDirectories);
+const getAppConfigs = R.curry(require('./get-app-configs'))(path, getDirectories);
 const { assert } = require('chai');
 
 describe('server apps get-app-configs -- integration', () => {
   let appsDir;
   let logger;
+  let server;
   let sandbox;
   const expected_app_prefixes = ['api'];
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
     appsDir = path.resolve(__dirname, path.join('../../tests/scenarios/multiple-apps-server', constants.APPS_FOLDER));
+    server = {root:path.resolve(__dirname, '../../tests/scenarios/multiple-apps-server')};
     logger = createLogger(sandbox);
   });
 
   context('when called with apps dir path', () => {
     it('should return two app configs', () => {
-      let configs = getMountConfigs(appsDir, logger);
+      let configs = getAppConfigs(appsDir, server, logger);
       assert.isTrue(configs.map((c) => c.prefix).includes('/api'), 'Did not recieved expected app api');
     });
   });
--- server/apps/get-app-configs.js
+++ server/apps/get-app-configs.js
@@ -1,9 +1,18 @@
 
-module.exports = function getAppConfigs(path, getDirectories, appsDir, logger) {
-  return getDirectories(appsDir, logger).map((directory) => {
+module.exports = function getAppConfigs(path, getDirectories, appsDir, server, logger) {
+  let appConfigs = getDirectories(appsDir, logger).map((directory) => {
     return {
       prefix: path.join('/', directory.name),
       root: directory.path
     }
   });
+
+  if(appConfigs.length === 0) {
+    appConfigs.push({
+      prefix: '/',
+      root: server.root
+    });
+  }
+
+  return appConfigs;
 }
