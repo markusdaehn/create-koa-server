c0cc25b43efa4ea444fdee44fbd67c280e2da70b
Markus Daehn <markus.daehn@outlook.com>
2017-01-01 19:06:05 -0800

    updated examples to take options object instead of config and logger

--- examples/basic/server/index.js
+++ examples/basic/server/index.js
@@ -2,4 +2,4 @@ const config = require('./config');
 const logger = require('../infrustructure/logging/logger')(config.env, config.app.name, config.app.logging);
 
 //module.exports = require('create-koa-server')(config, logger);
-module.exports = require('../../../server')(config, logger);
+module.exports = require('../../../server')({config, logger});
