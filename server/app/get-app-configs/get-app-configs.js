9b0615832cbecc3d6a64c43c995f51143321165e
Markus Daehn <markus.daehn@outlook.com>
2017-01-29 21:59:28 -0800

    refactored get-app-config to simplify code in prep to adding read app load feature

--- server/app/get-app-configs/get-app-configs.js
+++ server/app/get-app-configs/get-app-configs.js
@@ -1,32 +1,34 @@
-
 module.exports = function getAppConfigs(path, getDirectories, extendConfig, APPS_FOLDER, appRoots, logger) {
-  let appConfigs = [];
+  let configs = [];
 
+  // @NOTE: We could have mulitpe app folders containing definitions
+  // for the same app. This is to allow for extentions
   for(let i = 0; i < appRoots.length; i++) {
     let appsDir = path.join(appRoots[i], APPS_FOLDER);
-    let currentAppConfigs = getDirectories(appsDir, logger).map((directory) => {
-      return {
-        prefix: path.join('/', directory.name),
-        roots: [directory.path]
-      };
-    }).forEach((currentConfig) => {
-      let config = appConfigs.find((cfg) => { return cfg.prefix === currentConfig.prefix });
+    let directories = getDirectories(appsDir, logger);
+
+    directories.forEach((directory) => {
+      const prefix = path.join('/', directory.name)
+      const config = configs.find((cfg) => { return cfg.prefix === prefix });
 
       //@NOTE: This is temporary, we will have to really get the config and merge. For now, just override.
       if(config) {
         config.roots.push(directory.path);
       } else {
-        appConfigs.push(currentConfig);
+        configs.push({
+          prefix,
+          roots: [directory.path]
+        });
       }
     });
   }
 
-  if(appConfigs.length === 0) {
-    appConfigs.push({
+  if(configs.length === 0) {
+    configs.push({
       prefix: '/',
       roots: appRoots
     });
   }
 
-  return appConfigs;
+  return configs;
 }
