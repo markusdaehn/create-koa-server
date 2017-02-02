9db53b15f053164bbd508c559d7f699ae670e27d
Markus Daehn <markus.daehn@outlook.com>
2017-02-02 08:51:32 -0800

    added additional tests for get-configs to test root app

--- server/config/get-configs/get-configs.i-spec.js
+++ server/config/get-configs/get-configs.i-spec.js
@@ -2,9 +2,9 @@ const path = require('path');
 const getConfigs = require('./index');
 const { assert } = require('chai');
 
-describe.only('server.config.get-configs()', () => {
+describe('server.config.get-configs()', () => {
   const logger = createLogger()
-  describe('when passed server root with multiple apps', () => {
+  describe('when passed server root with app in the apps folder', () => {
     const serverRoot = path.resolve(__dirname, '../../../tests/scenarios/server/multiple-apps');
     const configs = getConfigs(serverRoot, logger)
 
@@ -19,7 +19,18 @@ describe.only('server.config.get-configs()', () => {
     it('should return a configs with only two keys', () => {
       assert.equal(Object.keys(configs).length, 2, 'The number of configs were not 2');
     });
+  });
 
+  describe('when passed server root with app at the root', () => {
+    const serverRoot = path.resolve(__dirname, '../../../tests/scenarios/server/basic');
+    const configs = getConfigs(serverRoot, logger)
+
+    it('should return a configs dictionary with /api defined', () => {
+      assert.isDefined(configs['/'], 'The /api config was not defined');
+    });
+    it('should return a configs with only 1 key', () => {
+      assert.equal(Object.keys(configs).length, 1, 'The number of configs were not 1');
+    });
   });
 });
 
