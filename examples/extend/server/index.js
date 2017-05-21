a21790d44f7446f48db0e24ceb3f5c40d49120c8
Markus Daehn <markus.daehn@outlook.com>
2017-05-21 11:28:22 -0700

    added commits in extend example to explain what is going on

--- examples/extend/server/index.js
+++ examples/extend/server/index.js
@@ -1,1 +1,3 @@
+//@NOTE: This taks the basic server with config and extends it using this this
+// servers config and defined routes. 
 module.exports = require('../../basic/server').create({serverRoot:__dirname});
